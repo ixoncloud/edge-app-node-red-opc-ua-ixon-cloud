867609e5042393f1cf40278f52527131d5423f64
Bart Gijsbertsen <bart.gijsbertsen@ixon.cloud>
2024-08-21 16:34:37 +0200

    Allow cross-platform builds
    This enables non-arm64 platform hosts to build the Docker image.

--- build_and_push_containers.sh
+++ build_and_push_containers.sh
@@ -1,2 +1,15 @@
-docker buildx build --platform linux/arm64/v8 --tag 192.168.140.1:5000/node-red-opc-ua:latest --push .
+#!/bin/bash
 
+# Output executed commands and stop on errors.
+set -e -x
+
+# Uncomment the following line should the edge gateway have been
+# given a different IP address.
+# docker buildx rm secure-edge-pro;
+
+# Create and initialize the build environment.
+docker buildx create --name secure-edge-pro \
+                     --config buildkitd-secure-edge-pro.toml
+docker buildx use secure-edge-pro
+
+docker buildx build --platform linux/arm64/v8 --tag 192.168.140.1:5000/node-red-opc-ua:latest --push .
